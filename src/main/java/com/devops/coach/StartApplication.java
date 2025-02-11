e09c5ce1ec7b158df403d0223b3ed1099ff5452a
ymuralisanthosh <122329941+ymuralisanthosh@users.noreply.github.com>
2025-02-11 15:02:09 +0530

    Update StartApplication.java

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Welcome folks..We are learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Jan 18th, 2025");
-        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
+        model.addAttribute("title", "Assignment done on 11-02-2025");
+        model.addAttribute("msg", "Assignment: Deploy Java application into EKS cluster using Helm & Jenkins Pipeline");
         return "index";
     }
 
